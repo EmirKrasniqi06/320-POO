c83aeb8ee9479d2c1c0981b3056d9bc2b488efda
Emir <154427659+EmirKrasniqi06@users.noreply.github.com>
2024-08-30 12:07:00 +0200

    Exercice 1 du 30.08.2024

--- personnel/Deplacement-Escargot/Deplacement-Escargot/Program.cs
+++ personnel/Deplacement-Escargot/Deplacement-Escargot/Program.cs
@@ -1,31 +1,65 @@
-﻿namespace Deplacement_Escargot
+﻿using Deplacement_Escargot;
+namespace Deplacement_Escargot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string escargot = "_@_ö";
-            Console.WriteLine(escargot);
-            int lifePoints = 50;
-            Console.SetCursorPosition(50, 5);
-            Console.WriteLine(lifePoints);
-            int verticalPosition = 10;
-            int horizontalPosition = 30;
-            Console.SetCursorPosition(horizontalPosition, verticalPosition);
-            Console.WriteLine(escargot);
-            Console.CursorVisible = false;
-            while (lifePoints > 0)
-            { 
-                Console.Clear();
-                Console.SetCursorPosition(50, 5);
-                Console.WriteLine("Points de vie : "+lifePoints);
-                horizontalPosition++;
-                Console.SetCursorPosition(horizontalPosition, verticalPosition);
-                Console.WriteLine(escargot);
-                lifePoints--;
-                Thread.Sleep(100); // Vitesse de l'exécution du programme
+            // Voir le fichier snailClass.cs
+
+            // Liste des escargots
+            List<snailClass> snails = new List<snailClass>();
+
+            // Demande à l'utilisateur le nombre d'escargots à afficher
+            Console.WriteLine("Combien d'escargot voulez-vous afficher ?");
+            int nbEscargot = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= nbEscargot; i++)
+            {
+                snailClass newSnail = new snailClass(starty: i + 2);
+                snails.Add(newSnail);
             }
-            Console.ReadLine();
+            foreach (snailClass snail in snails)
+            {
+                snail.Display();
+            }
+
+
+            //// Affichage escargot 1
+            //Console.SetCursorPosition(snail1.x, 10);
+            //Console.WriteLine(snail1.alive);
+
+            //// affichage escargot 2
+            //Console.SetCursorPosition(snail2.x, 10);
+            //Console.WriteLine(snail2.alive);
+
+            //while (snail1.lifePoints > 0)
+            //{
+            //    // Affichage des points de vie de l'escargot 1
+            //    Console.SetCursorPosition(50, 5);
+            //    Console.WriteLine("Points de vie : " + snail1.lifePoints);
+
+            //    // Affichage des points de vie de l'escargot 2
+            //    Console.SetCursorPosition(50, 5);
+            //    Console.WriteLine("Points de vie : "+ snail2.lifePoints);
+
+            //    // Methode qui ajoute +1 à la position horizontale
+            //    snail1.Move();
+            //    Console.SetCursorPosition(snail1.x, 10);
+            //    Console.WriteLine(snail1.alive);
+            //    Console.SetCursorPosition(snail1.x, 12);
+            //    Console.WriteLine(snail1.alive);
+            //    snail1.lifePoints--;
+            //    Thread.Sleep(50); // Vitesse de l'exécution du programme
+            //    Console.Clear();
+            //}
+            //Console.SetCursorPosition(50, 5);
+            //Console.WriteLine("L'escargot il est mort !");
+            //Console.SetCursorPosition(snail1.x, 10);
+            //Console.WriteLine(snail1.dead);
+            //Console.SetCursorPosition(snail1.x, 12);
+            //Console.WriteLine(snail1.dead);
+            //Console.ReadLine();
         }
     }
 }
---
+++ personnel/Deplacement-Escargot/Deplacement-Escargot/snailClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deplacement_Escargot
+{
+    internal class snailClass
+    {
+        // Attributs
+        public int lifePoints = 50;      // Vie du escargot
+        public int x = 30;               // Position horizontale
+        public int y = 10;               // Position verticale
+        public string alive = "_@_ö";    // Escargots vivant
+        public string dead = "____";     // Escargot 1 mort 
+        
+        // Constructeur
+        public snailClass(int starty)
+        {
+            y = starty;
+        }
+
+        // Méthodes 
+        public void Move()
+        {
+            x++;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{alive}");
+        }
+    }
+}
