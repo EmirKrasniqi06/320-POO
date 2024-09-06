324dccd2a241c04209fafc0f8bc4af876275cc3a
Emir <154427659+EmirKrasniqi06@users.noreply.github.com>
2024-09-06 10:58:57 +0200

    feat(parachutes): Faire bouger l'avion et l'afficher

--- personnel/ParaClub/ParaClub/Plane.cs
+++ personnel/ParaClub/ParaClub/Plane.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace ParaClub
 {
-    internal class Plane
+    internal class PlaneClass
     {
+        // Grandeur de la console
+        public const int WIDTH = 28;
+        public const int HEIGHT = 6;
+
+        // Dessin de l'avion sur une table
         private string[] view =
         {
             @" _                         ",
@@ -17,6 +22,45 @@ namespace ParaClub
             @"  \_______ --------- __>-} ",
             @"        \_____|_____/   |  "
         };
+
+        // Attributs de l'avion
+        public int x; // positionnement horizontale 
+        public int altitude; // Hauteur
+
+        /// <summary>
+        /// Constructeur
+        /// </summary>
+        public PlaneClass()
+        {
+            x = 0;
+            altitude = Config.SCREEN_HEIGHT;
+        }
+        /// <summary>
+        /// Affichage de chaque ligne de la table où se trouve l'avion
+        /// </summary>
+        public void PlaneDisplay()
+        {
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.SetCursorPosition(x, i);
+                Console.WriteLine(view[i]);
+            }
+        }
+        /// <summary>
+        /// Mouvement de l'avion vers droite si il ne se trouve pas dehor la console
+        /// </summary>
+        public void PlaneMovement()
+        {
+            if (x >= Config.SCREEN_WIDTH)
+            {
+                x = 0; // Si l'avion se trouve dehors la console retourner à la position initiale
+            }
+            else
+            {
+                x++; // Sinon bouger à droite
+            }
+        }
     }
+
 
 }
--- personnel/ParaClub/ParaClub/Program.cs
+++ personnel/ParaClub/ParaClub/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System.Numerics;
-
+using ParaClub;
 namespace ParaClub
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            // Initialiser la console
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+
+            // Déclaration et creation des objets
+            PlaneClass plane = new PlaneClass(); // Instancier un objet de type Plane dans la variable plane (qui fait partie de la classe Plane)
+
+            while (true)
+            {
+                plane.PlaneMovement();
+                
+
+                // Modifier ce que l'on *voit*
+                Console.Clear();
+                plane.PlaneDisplay();
+
+                // Temporiser
+                Thread.Sleep(100);
+            }
             Console.ReadLine();
         }
     }
